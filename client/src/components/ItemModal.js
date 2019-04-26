28be3bc59cbb45a70165df957588707497dcd5b1
ron.caruthers <ron.caruthers@industrieit.com>
2019-04-26 09:51:15 -0600

    finish tutorial, add auth for login, add and delete item

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Button,
   Modal,
@@ -8,15 +8,20 @@ import {
   FormGroup,
   Label,
   Input
-} from 'reactstrap';
-import { connect } from 'react-redux';
-import { addItem } from '../actions/itemActions';
+} from "reactstrap";
+import { connect } from "react-redux";
+import { addItem } from "../actions/itemActions";
+import PropTypes from "prop-types";
 
 class ItemModal extends Component {
   state = {
     modal: false,
-    name: ''
-  }
+    name: ""
+  };
+
+  static propTypes = {
+    isAuthenticated: PropTypes.bool
+  };
 
   toggle = () => {
     this.setState({
@@ -33,7 +38,7 @@ class ItemModal extends Component {
 
     const newItem = {
       name: this.state.name
-    }
+    };
 
     // Add item via addItem action
     this.props.addItem(newItem);
@@ -43,17 +48,20 @@ class ItemModal extends Component {
   };
 
   render() {
-    return(
+    return (
       <div>
-        <Button
-          color="dark"
-          style={{marginBottom: '2rem'}}
-          onClick={this.toggle}
-        >Add Item</Button>
-        <Modal
-          isOpen={this.state.modal}
-          toggle={this.toggle}
-        >
+        {this.props.isAuthenticated ? (
+          <Button
+            color="dark"
+            style={{ marginBottom: "2rem" }}
+            onClick={this.toggle}
+          >
+            Add Item
+          </Button>
+        ) : (
+          <h4 className="mb-3 ml-4">Please log in to manage items</h4>
+        )}
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
           <ModalHeader toggle={this.toggle}>Add to Shopping List</ModalHeader>
           <ModalBody>
             <Form onSubmit={this.onSubmit}>
@@ -66,22 +74,24 @@ class ItemModal extends Component {
                   placeholder="Add shopping item"
                   onChange={this.onChange}
                 />
-                <Button
-                  color="dark"
-                  style={{marginTop: '2rem'}}
-                  block
-                >Add item</Button>
+                <Button color="dark" style={{ marginTop: "2rem" }} block>
+                  Add item
+                </Button>
               </FormGroup>
             </Form>
           </ModalBody>
         </Modal>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => ({
-  item: state.item
-})
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated
+});
 
-export default connect(mapStateToProps, {addItem})(ItemModal);
+export default connect(
+  mapStateToProps,
+  { addItem }
+)(ItemModal);
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { connect } from 'react-redux';
-import { getItems, deleteItem } from '../actions/itemActions';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { connect } from "react-redux";
+import { getItems, deleteItem } from "../actions/itemActions";
+import PropTypes from "prop-types";
 
 class ShoppingList extends Component {
+  static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
+  };
 
   componentDidMount() {
     this.props.getItems();
@@ -13,25 +18,27 @@ class ShoppingList extends Component {
 
   onDeleteClick = id => {
     this.props.deleteItem(id);
-  }
+  };
 
   render() {
     const { items } = this.props.item;
-    return(
+    return (
       <Container>
         <ListGroup>
           <TransitionGroup className="shopping-list">
             {items.map(({ _id, name }) => (
               <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
-                <Button
-                  className="remove-btn"
-                  color="danger"
-                  size="sm"
-                  onClick={this.onDeleteClick.bind(this, _id)}
-                >
-                  &times;
-                </Button>
+                  {this.props.isAuthenticated ? (
+                    <Button
+                      className="remove-btn"
+                      color="danger"
+                      size="sm"
+                      onClick={this.onDeleteClick.bind(this, _id)}
+                    >
+                      &times;
+                    </Button>
+                  ) : null}
                   {name}
                 </ListGroupItem>
               </CSSTransition>
@@ -43,13 +50,9 @@ class ShoppingList extends Component {
   }
 }
 
-ShoppingList.propTypes = {
-  getItems: PropTypes.func.isRequired,
-  item: PropTypes.object.isRequired
-}
-
-const mapStateToProps = (state) => ({
-  item: state.item
+const mapStateToProps = state => ({
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(
