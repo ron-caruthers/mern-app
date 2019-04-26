d72e154462cee4ea860f90414460550efedb9873
ron.caruthers <ron.caruthers@industrieit.com>
2019-04-26 10:09:29 -0600

    fix capitalization users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const config = require("config");
-const jwt = require("jsonWebToken");
+const jwt = require("jsonwebtoken");
 const auth = require("../../middleware/auth");
 
 // User model
